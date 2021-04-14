869bc9156610fc37c48e3e7253ed913731e523c9
chinmaym07 <b418020@iiit-bh.ac.in>
2021-04-15 00:57:04 +0530

    Added 1.12 anecdote exercise step 1

--- part 1/part1-exercise/src/App.js
+++ part 1/part1-exercise/src/App.js
@@ -30,22 +30,29 @@ const Button = ({handleClick,text}) => (
     {text}
   </button>
 )
+
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-
+  const [selected, setSelected] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  const handleClick = e =>{
+    console.log((Math.random()*10).toFixed(0)%6);
+    setSelected((Math.random()*10).toFixed(0)%6);
+  }
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <div className="buttons">
-        <Button handleClick={()=> setGood(good+1)} text="Good" />
-        <Button handleClick={()=> setNeutral(neutral+1)} text="Neutral" />
-        <Button handleClick={()=> setBad(bad+1)} text="Bad" />
+      {anecdotes[selected]}
+      <div>
+        <button onClick={handleClick}>Next Anecdote </button>
       </div>
-     <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
+    
   )
 }
 
