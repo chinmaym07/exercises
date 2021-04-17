4ac95de7ae213759b85275d270901a1c24cf3d16
chinmaym07 <b418020@iiit-bh.ac.in>
2021-04-17 19:45:18 +0530

    Added Exercise 2.19 & 2.20 PhoneBook step 11 & 12

--- part 1/part1-exercise/src/App.js
+++ part 1/part1-exercise/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState ,useEffect } from 'react'
 import serverCom from './serverCom';
+import './index.css'
 
 
 const SearchComponent = ({handleSearch,search}) =>(
   <p>filter shown with : <input type="text" onChange={handleSearch} value={search} name="search"/></p>
 )
+
 
 const Person = ({person,handleDelete})=>{
 
@@ -29,27 +31,46 @@ const AddANewPersonConponent = ({handleChange,newPerson,handleSubmit})=> (
   </form>
 )
 
+
 const App = () => {
+  
   const [ persons, setPersons ] = useState([]) ;
   const [ newPerson, setNewPerson ] = useState({id:'',name:'',number:''})
   const [filteredPerson,setFilteredPerson] = useState([]);
   const [search, setSearch] = useState('');
+  const [notifMessage,setNotifMessage] = useState('');
+  const [typeOfNotif,setTypeNotifMessage] = useState(0);
 
   useEffect(()=>{
     serverCom
     .getAll()
     .then(data => setPersons(data))
+    .catch(error => {
+      setNotifMessage(error.message)
+      setTypeNotifMessage(-1);
+    });
+
   },[])
 
   const handleDelete = (id)=> {
     serverCom
     .deletePerson(id)
-    .then(data => console.log(data));
+    .then(data => {
+        let obj = persons.find(person => person.id === id);
+        setNotifMessage(`Information of ${obj.name} Deleted !!`);
+        setTypeNotifMessage(1);
+      })
+    .catch(error => {
+      setNotifMessage(error.message)
+      setTypeNotifMessage(-1);
+    });
+    setTimeout(() => {
+      setNotifMessage('')
+    }, 5000);
     setPersons(persons.filter(person => person.id !== id))
   }
   const handleChange = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
+
     setNewPerson({
       ...newPerson,
       [e.target.name] : e.target.value
@@ -60,6 +81,7 @@ const App = () => {
 
     setFilteredPerson(persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())));
   }
+
   const checkPresence= (obj)=> {
     for(let i = 0 ; i < persons.length;i++)
     {
@@ -82,8 +104,20 @@ const App = () => {
           }
           serverCom
           .update(id,newObj)
-          .then(data => setPersons(persons.map(person => person.id !== id ? person : data)));
+          .then(data => {
+            setPersons(persons.map(person => person.id !== id ? person : data))
+            setNotifMessage(`Phone Number of ${newPerson.name} Updated `);
+            console.log(`Phone Number of ${newPerson.name} Updated `);
+            setTypeNotifMessage(1);
+          })
+          .catch(error => {
+            setNotifMessage(`Information of ${newPerson.name} has already been removed from server !!`);
+            setTypeNotifMessage(-1);
+          });
           setNewPerson({name:'',number:''});    
+          setTimeout(() => {
+            setNotifMessage('')
+          }, 5000);
         }
           
       
@@ -91,8 +125,19 @@ const App = () => {
     else{
       serverCom
       .create(newPerson)
-      .then(data=>setPersons(persons.concat(data)));
-      
+      .then(data=>{
+        setPersons(persons.concat(data))
+        setNotifMessage(`Added ${newPerson.name}`);
+        console.log(`Added ${newPerson.name}`)
+        setTypeNotifMessage(1);
+      })
+      .catch(error => {
+        setNotifMessage(error.message)
+        setTypeNotifMessage(-1);
+      })
+      setTimeout(() => {
+        setNotifMessage('')
+      }, 5000);
       setNewPerson({name:'',number:''});
     }
     
@@ -100,7 +145,12 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      {
+        notifMessage !== '' ? <div className={typeOfNotif === 1 ? "post-notif":"neg-notif"}>
+        {notifMessage}
+        </div>:null
+      }
       <SearchComponent handleSearch={handleSearch} search={search} />
       <h1>Add a New</h1>
       <AddANewPersonConponent handleChange={handleChange} handleSubmit={handleSubmit} newPerson={newPerson}/>
