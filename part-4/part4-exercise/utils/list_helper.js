c0c74470bdbb6a400d43aaeac5156ae5dd38c192
chinmaym07 <b418020@iiit-bh.ac.in>
2021-04-29 02:56:14 +0530

    Added Exercise 4.5: helper functions and unit tests, step3

--- part-4/part4-exercise/utils/list_helper.js
+++ part-4/part4-exercise/utils/list_helper.js
@@ -11,8 +11,17 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce(reducer,0);
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (mx ,item)=> {
+        return Math.max(mx,item.likes) ;
+    }
+    let favrBlog =  blogs.length === 0 ? 0 : blogs.reduce(reducer,0);
+
+    return favrBlog == 0 ? {} : blogs.find(blog => blog.likes == favrBlog);
+};
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
